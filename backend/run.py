5ec91155b8eedec277509101e1e9407a950ef473
Roch D'Amour <roch.damour@gmail.com>
2019-03-03 21:08:14 -0500

    Uses 4 spaces indentation in python backend
    https://www.python.org/dev/peps/pep-0008/#indentation

--- backend/run.py
+++ backend/run.py
@@ -11,31 +11,31 @@ mongo = PyMongo(app)
 
 @app.route('/star', methods=['GET'])
 def get_all_stars():
-  star = mongo.db.stars
-  output = []
-  for s in star.find():
-    output.append({'name' : s['name'], 'distance' : s['distance']})
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    output = []
+    for s in star.find():
+        output.append({'name' : s['name'], 'distance' : s['distance']})
+    return jsonify({'result' : output})
 
 @app.route('/star/', methods=['GET'])
 def get_one_star(name):
-  star = mongo.db.stars
-  s = star.find_one({'name' : name})
-  if s:
-    output = {'name' : s['name'], 'distance' : s['distance']}
-  else:
-    output = "No such name"
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    s = star.find_one({'name' : name})
+    if s:
+        output = {'name' : s['name'], 'distance' : s['distance']}
+    else:
+        output = "No such name"
+    return jsonify({'result' : output})
 
 @app.route('/star', methods=['POST'])
 def add_star():
-  star = mongo.db.stars
-  name = request.json['name']
-  distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    name = request.json['name']
+    distance = request.json['distance']
+    star_id = star.insert({'name': name, 'distance': distance})
+    new_star = star.find_one({'_id': star_id })
+    output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+    return jsonify({'result' : output})
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3050, debug=True)
+    app.run(host='0.0.0.0', port=3050, debug=True)
