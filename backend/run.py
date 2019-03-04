40d78338486dcea88e7cedc94ac8224d58537350
Roch D'Amour <roch.damour@gmail.com>
2019-03-03 21:08:05 -0500

    Fixed backend server's port

--- backend/run.py
+++ backend/run.py
@@ -38,4 +38,4 @@ def add_star():
   return jsonify({'result' : output})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+  app.run(host='0.0.0.0', port=3050, debug=True)
